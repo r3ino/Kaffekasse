2bbc8be1c9b5caba3c52847e7a77d561bea25368
r3ino <reino.albrecht@protonmail.com>
2025-02-19 21:44:59 +0100

    Add initial project setup with core files and configurations
    Initialize the Kaffekasse project with essential files, including the Kasse class and its attributes. Include IDE-specific configuration files for project structure, version control, and Python setup. Add a placeholder image file (Kaffekasse.jpeg) for future use.

--- Kaffeekasse.py
+++ Kaffeekasse.py
@@ -1,5 +1,33 @@
+import datetime
+
 class Kasse:
     def __init__(self):
-        self.saldo = 0
+        self.saldo_in_cent = 0
         self.gesamtverbrauch = 0
         self.bestenliste = {}
+
+    def einzahlung(self, teilnehmer, betrag_in_cent, datum):
+        self.saldo_in_cent += betrag_in_cent
+        teilnehmer.einzahlungssumme += betrag_in_cent
+        teilnehmer.datum_d_letzten_einzahlung = datum
+        self.bestenliste_aktualisieren()
+
+    def einkauf(self, anzahl, betrag_in_cent):
+        self.saldo_in_cent -= betrag_in_cent
+        self.gesamtverbrauch += anzahl
+
+    def bestenliste_aktualisieren(self):
+        self.bestenliste = dict(sorted(self.bestenliste.items(), key=lambda item: item[1], reverse=True))
+
+
+class Teilnehmer:
+    def __init__(self, name):
+        self.datum_d_letzten_einzahlung = None
+        self.name = name
+        self.einzahlungssumme = 0
+
+    def tage_seit_letzter_einzahlung(self):
+        if self.datum_d_letzten_einzahlung is None:
+            return None
+        differenz = datetime.date.today() - self.datum_d_letzten_einzahlung
+        return differenz.days
